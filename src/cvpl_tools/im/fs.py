fa0f93454aa784519f8610c7ad9d47131e6856d0
Karl5766 <karl29029@gmail.com>
2024-08-17 15:56:27 -0400

    update docs

--- src/cvpl_tools/im/fs.py
+++ src/cvpl_tools/im/fs.py
@@ -447,3 +447,16 @@ class CacheDirectory(CachePath):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.remove_tmp()
+
+
+class MultiOutputStream:
+    def __init__(self, *files):
+        self.files = files
+
+    def write(self, message):
+        for file in self.files:
+            file.write(message)
+
+    def flush(self):
+        for file in self.files:
+            file.flush()
