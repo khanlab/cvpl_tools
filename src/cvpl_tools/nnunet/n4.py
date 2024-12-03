0ea9553ce1570fd059d64d68cfded79a49d3a2f1
Tianming Han <than83@robarts.ca>
2024-12-03 16:47:59 -0500

    update

--- src/cvpl_tools/nnunet/n4.py
+++ src/cvpl_tools/nnunet/n4.py
@@ -1,10 +1,30 @@
-import cvpl_tools.ome_zarr.io as ome_zarr_io
-import dask.array as da
+import ants
+import numpy.typing as npt
+import cvpl_tools.ome_zarr.io as ome_io
 
 
-image_netloc = 'gcs://khanlab-scratch/tmp/CacheDirectoryBlaze_F1A1Te4Blaze/GLOBAL_LABEL/os/global_os/dask_im'
-print(f'ome_io.load_dask_array_from_path from path {image_netloc}')
-group = ome_zarr_io.load_zarr_group_from_path(image_netloc, mode='r')
-print(ome_zarr_io.get_highest_downsample_level(group))
-# arr = da.from_array(group['0'])
-# print(arr.shape)
+def bias_correct(im: npt.NDArray, spline_param, shrink_factor, return_bias_field: bool = False) -> npt.NDArray:
+    """If return_bias_field is True, return the bias field instead of the corrected image
+
+    See ants.n4_bias_field_correction documentation
+
+    Examples
+        - bias_correct(im, spline_param=(16, ) * 3, shrink_factor=8, return_bias_field=return_bias_field)
+    """
+    im = ants.from_numpy(im)
+    imn4 = ants.n4_bias_field_correction(im,
+                                         spline_param=spline_param,
+                                         shrink_factor=shrink_factor,
+                                         return_bias_field=return_bias_field)
+    return imn4.numpy()
+
+
+def obtain_bias(im: npt.NDArray, write_loc=None) -> npt.NDArray:
+    """Returns a bias field numpy array that is of the same size and shape as the input
+
+    Corrected image can be obtained by computing im / obtain_bias(im)
+    """
+    bias = bias_correct(im, spline_param=(16,) * 3, shrink_factor=8, return_bias_field=True)
+    if write_loc is not None:
+        ome_io.write_ome_zarr_image(write_loc, f'{write_loc}.tmp', da_arr=bias, MAX_LAYER=1)
+    return bias
