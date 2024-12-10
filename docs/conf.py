e4ca594bd8743988fa3629bc9fb9a49e4cc90cfd
Karl5766 <karl29029@gmail.com>
2024-12-10 15:12:16 -0500

    0.8.1

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'cvpl_tools'
 copyright = '2024, KarlHanUW'
 author = 'KarlHanUW'
-release = '0.7.3'
+release = '0.8.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
